69815e388e948c61bd6698268de0e48cbe398a89
Ninoshark77 <200595@hkis.edu.hk>
2015-12-10 09:10:04 +0800

    added go left actions

--- gamedev.js
+++ gamedev.js
@@ -60,6 +60,82 @@ $( document ).ready(function() {
 		$("#goLeft1").show();
 	});
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	$( document ).ready(function() {
+
+	$("#goLeft1").click(function() {
+		$("#exploreBody").css("background-image", "url('img/spot2.jpg')");
+		$("#goRight1").hide();
+		$("#goLeft1").hide();
+		$("#goRight7").show();
+		$("#goLeft2").show();
+	});
+
+	$("#goLeft2").click(function() {
+		$("#exploreBody").css("background-image", "url('img/spot3.jpg')");
+		$("#goRight7").hide();
+		$("#goLeft2").hide();
+		$("#goRight6").show();
+		$("#goLeft3").show();
+	});
+
+	$("#goLeft3").click(function() {
+		$("#exploreBody").css("background-image", "url('img/spot4.gif')");
+		$("#goRight6").hide();
+		$("#goLeft3").hide();
+		$("#goRight5").show();
+		$("#goLeft4").show();
+
+	});
+
+	$("#goLeft4").click(function() {
+		$("#exploreBody").css("background-image", "url('img/spot5.jpg`')");
+		$("#goRight5").hide();
+		$("#goLeft4").hide();
+		$("#goRight4").show();
+		$("#goLeft5").show();
+	});
+
+	
+	$("#goLeft5").click(function() {
+		$("#exploreBody").css("background-image", "url('img/spot6.jpg')");
+		$("#goRight4").hide();
+		$("#goLeft5").hide();
+		$("#goRight3").show();]
+		$("#goLeft6").show();
+	});
+
+	$("#goLeft6").click(function() {
+		$("#exploreBody").css("background-image", "url('img/spot7.jpg')");
+		$("#goRight3").hide();
+		$("#goLeft6").hide();
+		$("#goRight2").show();]
+		$("#goLeft7").show();
+	});
+
+	$("#goLeft7").click(function() {
+		$("#exploreBody").css("background-image", "url('img/spot1.jpg')");
+		$("#goRight2").hide();
+		$("#goLeft7").hide();
+		$("#goRight1").show();]
+		$("#goLeft8").show();
+	});
+
 	
 
 });
